5b62d957b1b5c957a49f554520fd9edcb1fc5287
spalnique <lex.sydoruk@gmail.com>
2024-05-09 13:44:00 +0200

    upd

--- src/hooks/usePages.js
+++ src/hooks/usePages.js
@@ -4,12 +4,12 @@ const usePages = ({ initPage, initTotal } = { initPage: 1, initTotal: 0 }) => {
   const [page, setPage] = useState(initPage);
   const [totalPages, setTotalPages] = useState(initTotal);
 
-  const resetPage = useCallback(() => setPage(1), []);
-  const nextPage = useCallback(() => setPage((page) => page + 1), []);
+  const resetPage = () => setPage(1);
+  const nextPage = () => setPage((page) => page + 1);
 
-  const resetTotal = useCallback(() => setTotalPages(0), []);
+  const resetTotal = () => setTotalPages(0);
   const setTotal = useCallback((value) => setTotalPages(value), []);
-  
+
   return { page, totalPages, resetPage, nextPage, resetTotal, setTotal };
 };
 
